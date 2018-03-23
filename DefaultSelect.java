4846e7928ff917843de728ee6e0e71b4db08386c
Mayur Phadte <mayur.phadte@gmail.com>
2018-03-23 18:04:02 -0400

    Hello

--- DefaultSelect.java
+++ DefaultSelect.java
@@ -62,7 +62,7 @@ public abstract class DefaultSelect implements Comparable<Integer>{
         //printArray(myArray);
 
         //System.out.println(myArray[Math.floor((myArray.length + 1)/2)]);
-        int index = Math.floor((myArray.length+1)/2);
+        int index = (int)Math.floor((myArray.length+1)/2);
         System.out.println(myArray[index]);
     }
 }
