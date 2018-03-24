9fad38096aaa4e36eddd9d681909ccc90d95d61e
vjaswan <vjaswan@ncsu.edu>
2018-03-24 14:03:34 -0400

    final

--- DefaultSelect.java
+++ DefaultSelect.java
@@ -5,31 +5,28 @@ import java.util.Collections;
 import java.util.Scanner;
 
 
-public abstract class DefaultSelect implements Comparable<Integer>{
-    static int comparisons=0; //global variable to store number of comparisons
-    public static void main(String[] args) {
+public abstract class DefaultSelect implements Comparable<Integer>
+{
+    static int comparisons=0; 	//global variable to store number of comparisons
+    public static void main(String[] args) 
+    {
         Scanner inputScanner = new Scanner(System.in);
         ArrayList <Integer> input = new ArrayList<Integer>();
-        //int[] myArray= new int[length];
         int length = 0;
-        while (inputScanner.hasNext()){
+        while (inputScanner.hasNext())
+        {
           input.add(new Integer(inputScanner.nextInt()));
           length++;
         }
         int[] myArray= new int[length];
         inputScanner.close();
-        //read the array
-        /*
-        for(int i=0;i<length;i++)
-            myArray[i]=input.nextInt();
-        input.close();
-        */
-
-         //Record system time at start
+       
+        //Record system time at start
         long startTime = System.currentTimeMillis();
 
         //overidden function that uses the compare To function and increments the counter
-        Collections.sort(input, new Comparator<Integer>(){
+        Collections.sort(input, new Comparator<Integer>()
+        {
           //@Override
           public int compare(Integer a, Integer b){
             comparisons++;
@@ -39,12 +36,16 @@ public abstract class DefaultSelect implements Comparable<Integer>{
 
         //Record system time at end
         long endTime = System.currentTimeMillis();
+        
         //Difference is elapsed time and conver to integer.
         long elapsedTime = endTime - startTime;
+        
         //Check to see if long goes over max integer value
-        if(elapsedTime > Integer.MAX_VALUE){
+        if(elapsedTime > Integer.MAX_VALUE)
+        {
           throw new IllegalArgumentException(elapsedTime + "cannot convert to int without modifying value");
         }
+        
         //Typecast to integer and write to variable
         int runtime = (int) elapsedTime;
         //Write runtime and key comparisons to standard error
@@ -56,13 +57,9 @@ public abstract class DefaultSelect implements Comparable<Integer>{
         for(Integer intobject : input){
           myArray[i++] = intobject.intValue();
         }
-        //printArray(myArray);
-
-        //System.out.println(myArray[Math.floor((myArray.length + 1)/2)]);
+        
         int kIndex = (int)Math.floor((myArray.length + 1)/2);
         int index = kIndex - 2;
-        //System.out.println("median" + myArray[kIndex + 1]);
         System.out.println("median," + myArray[kIndex - 1]);
-        //System.out.println("median," + myArray[kIndex]);
     }
 }
