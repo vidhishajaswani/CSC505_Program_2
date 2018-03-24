eb70b768704a63cb567e47faabeffb5b9018c681
Mayur Phadte <mayur.phadte@gmail.com>
2018-03-24 17:39:24 -0400

    FINALLL

--- quickselect.java
+++ quickselect.java
@@ -1,65 +1,91 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
+
+class Elements{
+	int value;
+	int index;
+	Elements(int a, int b){
+		this.value = a;
+		this.index = b;
+	}
+}
 public class quickselect {
 
-	int partition(int arr[], int a, int b){
-		int pivot;
+	int giveCorrectIndex(int arr[], int a, int b){
+		int pivotIndex = 0;
 
-		//Pivot Selection
 		if((b-a+1) < 9){
-			pivot = arr[a];
-		}else {
-			int middle = (b-a)/2;
-
-			int temp[] = {arr[a], arr[b], arr[middle]};
-			Arrays.sort(temp);
-			pivot = temp[1];
+			pivotIndex = b;
 		}
+		else{
+			ArrayList <Elements> em = new ArrayList<Elements>();
+			em.add(new Elements(arr[a], a));
+			em.add(new Elements(arr[b], b));
+			em.add(new Elements(arr[(b-a)/2], (b-a)/2));
 
-		//Quicksort begins
+			Collections.sort(em, new Comparator<Elements>(){
+			          //@Override
+			          public int compare(Elements a, Elements b){
+									if(a.value > b.value){
+										return 1;
+									}else{
+										return -1;
+									}
+			          }
+			        });
+			int[] valueArray = new int[3];
+			int[] indexArray = new int[3];
+			int i = 0;
+			for(Elements e : em){
+				valueArray[i] = e.value;
+				indexArray[i] = e.index;
+			}
 
+			System.out.println("before :" + Arrays.toString(arr));
+			int indexToBeSwapped = indexArray[1];
+			int temp = arr[b];
+			arr[b] = arr[indexToBeSwapped];
+			arr[indexToBeSwapped] = temp;
+			System.out.println("after :" + Arrays.toString(arr));
+			pivotIndex = b;
+		}
+		int temp = 0;
 		int i = a - 1;
 		for(int j = a; j < b; j++){
-			if(arr[j] <= pivot){
+			if(arr[j] < arr[pivotIndex]){
 				i++;
-				int temp = arr[i];
+				temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
+				//System.out.println("andar " + Arrays.toString(arr));
 			}
 		}
-		int temp = arr[i+1];
-		arr[i+1] = arr[b];
-		arr[b] = temp;
+		temp = arr[i+1];
+		arr[i+1] = arr[pivotIndex];
+		arr[pivotIndex] = temp;
+		//System.out.println("bahar " + Arrays.toString(arr));
+		return i + 1;
 
-		if((i+1) == (arr.length-1)/2){
-			System.err.println(i+1);
-			System.out.println("median," + arr[i+1]);
-			return 0;
-		}else{
-			return i+1;
-		}
+
 	}
 
-	int quickselect(int arr[], int a, int b){
+	void quickselect(int arr[], int a, int b){
 		int medianIndex = (arr.length-1)/2;
-		System.err.println(a + " vs " + b);
 		if(a <= b){
-			int index = partition(arr, a, b);
-			if(index == 0){
-				return 0;
-			} else if(index > medianIndex){
-				return quickselect(arr, a, index - 1);
-			}else{
-				return quickselect(arr, index + 1, b);
-			}
+			int index = giveCorrectIndex(arr, a, b);
 
+			if(index == medianIndex){
+				System.out.println("median," + arr[index]);
+			}
+			else if(index > medianIndex){
+				quickselect(arr, a, index - 1);
+			}else{
+				quickselect(arr, index + 1, b);
+			}
 		}
-		System.err.println(a + " > " + b);
-		return -1;
 
 	}
+
 	public static void main(String args[])
 	{
 		Scanner inputScanner = new Scanner(System.in);
@@ -75,17 +101,27 @@ public class quickselect {
 		for(Integer intobject : input){
 			arr[i++] = intobject.intValue();
 		}
-
 		quickselect qs = new quickselect();
-		int answer = qs.quickselect(arr, 0, n-1);
-		//int answer = quickselect(arr, 0, n-1);
-		//System.err.println("index," + answer);
-		//System.err.println("aaju," + arr[answer-1]);
-		//System.err.println("baju," + arr[answer+1]);
-		//System.out.println("median," + arr[answer]);
-
+		qs.quickselect(arr, 0, n-1);
+		System.out.println(Arrays.toString(arr));
 	}
 
 
 
 }
+
+//Input
+/*Scanner inputScanner = new Scanner(System.in);
+arrList <Integer> input = new arrList<Integer>();
+int n = 0;
+while (inputScanner.hasNext()){
+	input.add(new Integer(inputScanner.nextInt()));
+	n++;
+}
+inputScanner.close();
+int[] arr= new int[n];
+int i = 0;
+for(Integer intobject : input){
+	arr[i++] = intobject.intValue();
+}
+*/
