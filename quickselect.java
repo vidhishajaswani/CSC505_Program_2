c0b524b1cc01be096940ede33d018ea8f98bc2bb
vjaswan <vjaswan@ncsu.edu>
2018-03-24 21:38:16 -0400

    holaaa

--- quickselect.java
+++ quickselect.java
@@ -16,6 +16,9 @@ class Elements{
 }
 public class quickselect {
 
+	static int comparisons=0;
+	static Integer x, y;
+
 	int giveCorrectIndex(int arr[], int a, int b){
 		int pivotIndex = 0;
 
@@ -44,11 +47,12 @@ public class quickselect {
 			for(Elements e : em){
 				valueArray[i] = e.value;
 				indexArray[i] = e.index;
-				//i++;
+				i++;
 			}
 
 			//System.out.println("before :" + Arrays.toString(arr));
-			int indexToBeSwapped = indexArray[1];
+			//int indexToBeSwapped = indexArray[1];
+			int indexToBeSwapped = a+(b-a)/2;
 			int temp = arr[b];
 			arr[b] = arr[indexToBeSwapped];
 			arr[indexToBeSwapped] = temp;
@@ -59,15 +63,25 @@ public class quickselect {
 		int i = a - 1;
 		for(int j = a; j < b; j++){
 			if(arr[j] < arr[pivotIndex]){
+				//System.out.println("a[j]  :" +arr[j]);
+
 				i++;
 				temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
 			}
 		}
+		//System.out.println("a  :" +a+" val of a: "+arr[a]+"  b: "+b+"  val of b: "+arr[b]);
+		//System.out.println("b-a/2"+(b-a)/2);
+
+		//System.out.println("pivot  :" +arr[pivotIndex]);
 		temp = arr[i+1];
 		arr[i+1] = arr[pivotIndex];
 		arr[pivotIndex] = temp;
+		//System.out.println("pivot swapp :" + Arrays.toString(arr));
+		//System.out.println("pivot  :" +arr[pivotIndex]);
+
+
 		return i + 1;
 
 
@@ -107,10 +121,35 @@ public class quickselect {
 			arr[i++] = intobject.intValue();
 		}
 		quickselect qs = new quickselect();
+        long startTime = System.currentTimeMillis();
 		int index=qs.quickselect(arr, 0, n-1);
+        long endTime = System.currentTimeMillis();
+
+        long elapsedTime = endTime - startTime;
+
+        if(elapsedTime > Integer.MAX_VALUE)
+        {
+          throw new IllegalArgumentException(elapsedTime + "cannot convert to int without modifying value");
+        }
+        int runtime = (int) elapsedTime;
+		//System.out.println("main :" + Arrays.toString(arr));
+
 		System.out.println("median," + arr[index]);
+		System.out.println("index," + index);
+		
+        System.out.println("runtime," + runtime);
+
 
 		//System.out.println(Arrays.toString(arr));
+	}
+	
+	//overridden function compareTo that increments the global counter
+	public static int compare(int l,int r)
+	{
+		comparisons++;
+		x = Integer.valueOf(l);
+		y = Integer.valueOf(r);
+		return x.compareTo(y);
 	}
 
 
